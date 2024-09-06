c21c8fea764d0cf9cd614fd30ae5fda4eb16f96d
Maksim Leskin <Maskim.Leskin@methed.ru>
2024-09-06 16:23:41 +0300

    add change xp

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// Функция для вычисления XP по кубической формуле
-function calculateXP(level, a, b, c) {
-  return a + b * level ** 2 + c * level ** 3;
+// Функция для вычисления XP по линейной формуле
+function linearXP(level, a, b) {
+  return a + b * level;
 }
 
 // Параметры
-const a = 200;
-const b = 20;
-const c = 3;
+const a = 200; // XP на уровне 0
+const b = 6600 / 29; // Прирост XP на каждом уровне
 
-// Вычисление XP для уровней с 0 по 29
+// Вывод XP для уровней с 0 по 29 с округлением до ближайших 100
 for (let level = 0; level <= 29; level++) {
-  const xp = calculateXP(level, a, b, c);
-  console.log(`level_xp[${level}]: ${xp}`);
+  const xp = linearXP(level, a, b);
+  console.log(`level_xp[${level}]: ${Math.round(xp / 100) * 100}`);
 }
