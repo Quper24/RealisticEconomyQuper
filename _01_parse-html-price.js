9bc16cc51c9a9f9a04262069ad4876cfc74fb623
Maksim Leskin <quper2405@gmail.com>
2026-07-03 00:13:26 +0300

    upd

--- _01_parse-html-price.js
+++ _01_parse-html-price.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 // URL к твоему локальному серверу
-const url = "http://192.168.0.7:8081";
+const url = "http://192.168.0.14:8081/";
 
 // Путь для сохранения JSON
 const outputFile = "./diesel_prices.json";
